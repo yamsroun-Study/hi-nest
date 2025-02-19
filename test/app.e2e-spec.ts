a3dec4766ff2567d819ac58654ae74772582b04e
yamsroun <yamsroun@gmail.com>
2025-02-19 18:05:45 +0900

    4.1 Testing GET movies id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
@@ -7,12 +7,19 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -45,6 +52,21 @@ describe('AppController (e2e)', () => {
     it('DELETE', () => {
       return request(app.getHttpServer())
         .delete('/movies')
+        .expect(404);
+    });
+  });
+
+  describe('/movies/:id', () => {
+    it.todo('TODO');
+    it('GET 200', () => {
+      return request(app.getHttpServer())
+        .get('/movies/1')
+        .expect(200);
+    });
+
+    it('GET 404', () => {
+      return request(app.getHttpServer())
+        .get('/movies/999')
         .expect(404);
     });
   });
